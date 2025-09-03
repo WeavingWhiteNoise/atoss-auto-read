8ce575db980e64e9be4ea9978c06d446a4c68bb6
WeavingWhiteNoise <marius.teppe@horiba.com>
2025-09-03 09:27:24 +0200

    Update atoss-auto-read-main.user.js

--- atoss-auto-read-main.user.js
+++ atoss-auto-read-main.user.js
@@ -88,7 +88,7 @@ fileInput.addEventListener('change', (event) => {
             if (i > sheetData.length-1) {
                 clearInterval(interval);
             }
-        }, 1000); // Wait for 1000ms (1 seconds) between each iteration
+        }, 2000); // Wait for 1000ms (1 seconds) between each iteration
 
 
 
