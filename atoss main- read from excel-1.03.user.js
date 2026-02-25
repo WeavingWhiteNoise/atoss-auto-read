ed8a2665be5199d09a8027c01796a7d2e2d32dbd
WeavingWhiteNoise <marius.teppe@horiba.com>
2026-02-25 15:27:46 +0100

    Add files via upload

---
+++ atoss main- read from excel-1.03.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         atoss main: read from excel
+// @namespace    http://tampermonkey.net/
+// @version      1.03
+// @description  Lädt das eigentliche Script von zentraler URL
+// @match        https://he-atoss.horiba.eu:5000/*
+// @grant        GM_xmlhttpRequest
+// @require      https://raw.githubusercontent.com/WeavingWhiteNoise/atoss-auto-read/main/atoss-auto-read-main.user.js?v=1.03
+// @require      https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js
+// @updateURL    https://raw.githubusercontent.com/WeavingWhiteNoise/atoss-auto-read/main/atoss-auto-read-main.user.js
+// @downloadURL  https://raw.githubusercontent.com/WeavingWhiteNoise/atoss-auto-read/main/atoss-auto-read-main.user.js
+// ==/UserScript==
---
+++ atoss sub- read from excel-1.03.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         atoss sub: read from excel
+// @namespace    http://tampermonkey.net/
+// @version      1.03
+// @description  Lädt das eigentliche Script von zentraler URL
+// @match        https://he-atoss.horiba.eu:5000/Home/AddOrEdit?date=*
+// @grant        GM_xmlhttpRequest
+// @require      https://raw.githubusercontent.com/WeavingWhiteNoise/atoss-auto-read/main/atoss-auto-read-sub.user.js?v=1.03
+// @require      https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js
+// @updateURL    https://raw.githubusercontent.com/WeavingWhiteNoise/atoss-auto-read/main/atoss-auto-read-sub.user.js
+// @downloadURL  https://raw.githubusercontent.com/WeavingWhiteNoise/atoss-auto-read/main/atoss-auto-read-sub.user.js
+// ==/UserScript==
