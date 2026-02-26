e6a742f92662249a40840f91d16b1d64cd6e7ae1
WeavingWhiteNoise <marius.teppe@horiba.com>
2026-02-26 09:24:00 +0100

    Update script description for clarity

--- tampermonkey_script_atoss_sub.js
+++ tampermonkey_script_atoss_sub.js
@@ -2,7 +2,7 @@
 // @name         tampermonkey_script_atoss_main
 // @namespace    http://tampermonkey.net/
 // @version      1.03
-// @description  load github code from url
+// @description  load github script via url
 // @match        https://he-atoss.horiba.eu:5000/Home/AddOrEdit?date=*
 // @grant        GM_xmlhttpRequest
 // @require      https://raw.githubusercontent.com/WeavingWhiteNoise/atoss-auto-read/main/atoss-auto-read-sub.user.js?v=1.03
